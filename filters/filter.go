89e28357db4ebd7b5c0677bdfd4169c51ca64ef4
sh4869 <sh4869221b@gmail.com>
2020-09-20 01:46:02 +0900

    update filter

--- filters/filter.go
+++ filters/filter.go
@@ -7,6 +7,7 @@ import (
 	"github.com/astaxie/beego/context"
 )
 
+// LogManager is log output function
 var LogManager = func(ctx *context.Context) {
 	fmt.Println("IP::" + ctx.Request.RemoteAddr + ", Url::" + ctx.Request.RequestURI + ", Time::" + time.Now().Format(time.RFC3339Nano))
 }
